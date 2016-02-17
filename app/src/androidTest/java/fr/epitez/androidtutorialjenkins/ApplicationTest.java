5efe365827bde84b49c6892d5dd666d3904dbda8
JF Marronnier <jfmarronnier@epitez.fr>
2016-02-17 10:48:15 +0100

    Exemple de teste Ko

--- app/src/androidTest/java/fr/epitez/androidtutorialjenkins/ApplicationTest.java
+++ app/src/androidTest/java/fr/epitez/androidtutorialjenkins/ApplicationTest.java
@@ -5,6 +5,8 @@ import android.content.pm.PackageInfo;
 import android.test.ApplicationTestCase;
 import android.test.MoreAsserts;
 
+import junit.framework.Assert;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
@@ -27,4 +29,10 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         assertNotNull(info);
         MoreAsserts.assertMatchesRegex("\\d\\.\\d", info.versionName);
     }
+
+    public void testEnCours() throws Exception {
+        PackageInfo info = application.getPackageManager().getPackageInfo(application.getPackageName(), 0);
+        assertEquals (1,2);
+    }
+
 }
